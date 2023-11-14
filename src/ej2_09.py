b3fc04e1443ae9c34e9b37eea888b17c2f44a278
smorcor744 <mcsergio@iesrafaelalberti.es>
2023-11-14 19:08:29 +0100

    update

--- src/ej2_09.py
+++ src/ej2_09.py
@@ -2,14 +2,18 @@
 
 
 def pedir_palabra() -> list:
-    palabra = tuple(input("Introduce una palabra: "))
-    return palabra
+    palabra = input("Introduce una palabra: ").strip().lower()
+    palabra = palabra.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u")
+    return tuple(palabra)
 
 
 def cont_vocales(palabra: tuple) -> tuple:
     vocales = (["a",0],["e",0],["i",0],["o",0],["u",0])
+
     for vocal in vocales:
         vocal[1] = palabra.count(vocal[0])
+        print(vocal[0] + " = " + str(vocal[1]))
+
     return vocales
 
 
