d188989b47bbc1c92b083d7427b53c4c0c8b3234
smorcor744 <mcsergio@iesrafaelalberti.es>
2023-11-27 01:34:27 +0100

    update

--- src/conjuntos3_06.py
+++ src/conjuntos3_06.py
@@ -5,11 +5,6 @@ Crea un conjunto consonantes que contenga las letras del alfabeto que no son voc
 Crea un conjunto letras_comunes que contenga las letras que están tanto en el conjunto vocales como en el conjunto consonantes.
 Estos ejercicios te ayudarán a practicar y comprender mejor cómo trabajar con conjuntos en Python. ¡Espero que te sean útiles! Si tienes alguna pregunta o necesitas más ejercicios, no dudes en decírmelo."""
 
-
-
-
-
-
 def main():
     vocales = {'a', 'e', 'i', 'o', 'u'}
     alfabeto = set('abcdefghijklmnopqrstuvwxyz')
